3431eb87c20fa5f6a74a6f56ea1aac9eff53bf20
affishaikh <sk.aftab1999@gmail.com>
2020-05-13 08:47:28 +0530

    Create endpoint to get quotes by author

--- src/main/kotlin/com/learning/quotes_service/controllers/QuotesController.kt
+++ src/main/kotlin/com/learning/quotes_service/controllers/QuotesController.kt
@@ -3,6 +3,7 @@ package com.learning.quotes_service.controllers
 import com.learning.quotes_service.domain.Quote
 import com.learning.quotes_service.repository.QuoteRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 
@@ -10,7 +11,12 @@ import reactor.core.publisher.Flux
 class QuotesController(private val quotesRepository: QuoteRepository) {
 
     @GetMapping("/quotes")
-    fun getTodo(): Flux<Quote> {
+    fun getQuotes(): Flux<Quote> {
         return quotesRepository.findAll();
+    }
+
+    @GetMapping("/quotes/author")
+    fun getQuotesByAuthor(@RequestParam name: String): Flux<Quote> {
+        return quotesRepository.findByAuthor(name);
     }
 }
--- src/main/kotlin/com/learning/quotes_service/repository/QuoteRepository.kt
+++ src/main/kotlin/com/learning/quotes_service/repository/QuoteRepository.kt
@@ -2,5 +2,8 @@ package com.learning.quotes_service.repository
 
 import com.learning.quotes_service.domain.Quote
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 
-interface QuoteRepository : ReactiveCrudRepository<Quote, Int> {}
+interface QuoteRepository : ReactiveCrudRepository<Quote, Int> {
+    fun findByAuthor(author: String): Flux<Quote>
+}
